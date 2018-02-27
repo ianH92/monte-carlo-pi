289260a77ba7194f6a01e4fd3248132715cce59e
ianH92 <>
2018-02-26 20:05:36 -0800

    circle on canvas, start button, radius and center

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 
-ctx.fillRect(10, 10, 100, 100);
+let width = canvas.width;
+let height = canvas.height;
+let centerX = (width / 2);
+let centerY = (height / 2);
+let radius = (width <= height) ? centerX : centerY;
+
+//Draw Circle
+ctx.beginPath();
+ctx.arc(centerX, centerY, radius, 0, (2 * Math.PI));
+ctx.stroke();
+
+let startButon = document.getElementById('startButton');
+
+
 console.log('Hello, World!');
+
